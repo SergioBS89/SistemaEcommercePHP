33faccd3b1ce1c33f21e0174598d214e69eecb14
sergio <sergiovencillon10@gmail.com>
2022-03-17 14:51:08 +0100

    searcher

--- ecommerceApp/views/modules/descriptionProduct.php
+++ ecommerceApp/views/modules/descriptionProduct.php
@@ -1,1 +1,43 @@
-<h1>Description product</h1>
+<div class="container-fluid well well-sm barProducts">
+
+	<div class="container">
+		
+		<div class="row">
+
+			<div class="col-xs-12 viewProducts">	
+				
+
+				
+				<div class=" col-xs-6">	
+					  
+					<!-- /* -------------------------------------------------------------------------- */
+					/*                                 BREADCRUMBS                                */
+					/* -------------------------------------------------------------------------- */ -->
+						<ul class="text-uppercase" style="margin: 0;">			            	
+			            	<li class="bread"><a style="color: red;" href="<?php echo $StaticUrl;?>">HOME</a></li>                 
+			            </ul>
+			    </div>
+				<div  class="col-xs-6 titleSectionProducts">
+					
+				</div>		    
+
+			</div>
+		</div>
+	</div>
+</div>
+<!-- 
+ /* -------------------------------------------------------------------------- */
+ /*                       SECCION INFORMACION DEL PRODUCO                      */
+ /* -------------------------------------------------------------------------- */ -->
+
+ <div class="container-fluid">
+     <div class="container">
+         <div class="row">
+             <div class="col-md-5 col-sm-6 col-xs-12 zoomImg">
+                 <div>
+                     
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
